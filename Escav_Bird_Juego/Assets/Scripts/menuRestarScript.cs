d8d0833e94d22bd0417df81d9e9f590a6dae5132
AnicetoB <aniceto.bokari.egea@gmail.com>
2014-10-23 19:58:32 +0200

    Guardado 5 - Menú Restart resuelto

---
+++ Escav_Bird_Juego/Assets/Scripts/menuRestarScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuRestarScript : MonoBehaviour {
+
+	Animator animacion_panel;
+	
+	// Use this for initialization
+	void Start () {
+		animacion_panel = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameControl.dead) {
+			animacion_panel.SetBool ("menuRestartOn", true);
+		}
+	}
+
+	public void restart(){
+		animacion_panel.SetBool ("menuRestartOn", false);
+		Application.LoadLevel ("Nivel_unico");
+		GameControl.dead = false;
+	}
+
+	public void quit(){
+		Application.Quit ();
+	}
+}
